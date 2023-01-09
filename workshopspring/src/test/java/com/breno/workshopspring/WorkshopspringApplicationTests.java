27ca2455d55501e24376e5832dca8334e96ef840
Breno Barreto <brenobarreto5@hotmail.com>
2023-01-09 11:52:43 -0300

    created project

---
+++ workshopspring/src/test/java/com/breno/workshopspring/WorkshopspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breno.workshopspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
