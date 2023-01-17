dba82e32e10cdfddb177c6976e66072f7fadc412
Breno Barreto <brenobarreto5@hotmail.com>
2023-01-17 10:19:53 -0300

    add comments in posts

--- workshopspring/src/main/java/com/breno/workshopspring/config/Instantiation.java
+++ workshopspring/src/main/java/com/breno/workshopspring/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.breno.workshopspring.domain.Post;
 import com.breno.workshopspring.domain.User;
 import com.breno.workshopspring.dto.AuthorDTO;
+import com.breno.workshopspring.dto.CommentDTO;
 import com.breno.workshopspring.repository.PostRepository;
 import com.breno.workshopspring.repository.UserRepository;
 
@@ -40,6 +41,13 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("25/01/2023"), "Partiu Viagem!", "Vou viajar para Rio de Contas", new AuthorDTO(pablo));
 		Post post2 = new Post(null, sdf.parse("17/03/2023"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(pablo));
+		
+		CommentDTO comment1 = new CommentDTO("Boa viagem mano!", sdf.parse("25/01/2023"), new AuthorDTO(joao));
+		CommentDTO comment2 = new CommentDTO("Aproveite", sdf.parse("25/01/2023"), new AuthorDTO(maria));
+		CommentDTO comment3 = new CommentDTO("Tenha um ótimo dia", sdf.parse("17/03/2023"), new AuthorDTO(maria));
+		
+		post1.getComments().addAll(Arrays.asList(comment1, comment2));
+		post2.getComments().addAll(Arrays.asList(comment3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
