5f322182ceae998dd02d25c93caca762a5be8cc7
Breno Barreto <brenobarreto5@hotmail.com>
2023-01-16 21:36:12 -0300

    create authorDTO

--- workshopspring/src/main/java/com/breno/workshopspring/config/Instantiation.java
+++ workshopspring/src/main/java/com/breno/workshopspring/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.breno.workshopspring.domain.Post;
 import com.breno.workshopspring.domain.User;
+import com.breno.workshopspring.dto.AuthorDTO;
 import com.breno.workshopspring.repository.PostRepository;
 import com.breno.workshopspring.repository.UserRepository;
 
@@ -35,12 +36,12 @@ public class Instantiation implements CommandLineRunner {
 		User pablo = new User(null, "Pablo Sarabia", "pablo@gmail.com");
 		User joao = new User(null, "João Brabo", "joaobrabo@hotmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("25/01/2023"), "Partiu Viagem!", "Vou viajar para Rio de Contas", pablo);
-		Post post2 = new Post(null, sdf.parse("17/03/2023"), "Bom dia!", "Acordei feliz hoje!", pablo);
-		
-		postRepository.saveAll(Arrays.asList(post1, post2));
 		userRepository.saveAll(Arrays.asList(maria, pablo, joao));
 		
+		Post post1 = new Post(null, sdf.parse("25/01/2023"), "Partiu Viagem!", "Vou viajar para Rio de Contas", new AuthorDTO(pablo));
+		Post post2 = new Post(null, sdf.parse("17/03/2023"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(pablo));
+		
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
