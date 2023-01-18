6804f021692f158576205a22306b84f1101db94b
Breno Barreto <brenobarreto5@hotmail.com>
2023-01-18 11:31:11 -0300

    implements fullsearch with various criteria

--- workshopspring/src/main/java/com/breno/workshopspring/resources/PostResources.java
+++ workshopspring/src/main/java/com/breno/workshopspring/resources/PostResources.java
@@ -1,5 +1,6 @@
 package com.breno.workshopspring.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +32,18 @@ public class PostResources {
 	public ResponseEntity<List<Post>> findByTitleContaining(@RequestParam(value="text", defaultValue="") String text) {
 		text = URL.decodeParam(text);
 		List<Post> posts = service.findByTitleContaining(text);
+		return ResponseEntity.ok().body(posts);
+	}
+	
+	@GetMapping("/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value="text", defaultValue="") String text,
+			@RequestParam(value="minDate", defaultValue="") String minDate,
+			@RequestParam(value="maxDate", defaultValue="") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> posts = service.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(posts);
 	}
 }
